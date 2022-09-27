3f2006ad70284c8d01c154ad4c18e467b55d1d0b
CarlosLTIV <clozanotrivino@gmail.com>
2022-09-26 22:29:54 -0500

    html y css

--- src/app/components/rg-table-management/rg-table-management.component.ts
+++ src/app/components/rg-table-management/rg-table-management.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { TableManagementGetAllService } from 'src/app/services/table-management-get-all.service';
 
 
@@ -8,7 +9,14 @@ import { TableManagementGetAllService } from 'src/app/services/table-management-
   styleUrls: ['./rg-table-management.component.scss']
 })
 export class RgTableManagementComponent implements OnInit {
-
+  createTableForm= new FormGroup({
+    numberTable: new FormControl("",[Validators.required]),
+    nPersonTable: new FormControl("",[Validators.required])
+  });
+  searchTableForm= new FormGroup({
+    idTableS: new FormControl("",[Validators.required])
+  });
+  
  
   displayedColumns: string[] = ['_id', 'number', 'numberp'];
   dataSource: any[] = [];
@@ -21,6 +29,19 @@ export class RgTableManagementComponent implements OnInit {
       console.log(this.dataSource);
       this.dataSource = this.dataSource;
     })
+  }
+  createTableSubmit(){
+
+  }
+  searchTableSubmit(){
+    
+  }
+  
+  isInvalidField(field: string){
+    return this.createTableForm.get(field)?.invalid && (this.createTableForm.get(field)?.dirty || this.createTableForm.get(field)?.touched);
+  }
+  hasError(field: string, validation: string){
+    return this.createTableForm.get(field)?.hasError(validation);
   }
 
 }
