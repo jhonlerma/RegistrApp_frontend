4a6b69d601c34360ae2743168c784dadb7db0988
PaulaRosero <parosero@unicauca.edu.co>
2022-09-28 22:07:16 -0500

    Avance candidate

--- src/app/components/rg-candidate-management/rg-candidate-management.component.ts
+++ src/app/components/rg-candidate-management/rg-candidate-management.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { MatTableDataSource } from '@angular/material/table';
+import { User } from 'src/app/models/candidate';
 
 @Component({
   selector: 'app-rg-candidate-management',
@@ -7,9 +10,40 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RgCandidateManagementComponent implements OnInit {
 
+  candidateList: Candidate[] = [];
+
+  createCandidateForm = new FormGroup({
+    document: new FormControl("", [Validators.required]),
+    last_name: new FormControl("",[Validators.required]),
+    name: new FormControl("", [Validators.required]),
+    resolution: new FormControl("", [Validators.required]),
+    // ??political_party: new FormControl("", [Validators.required])
+  });
+
+  displayedColumns: string[] = ['position', 'name', 'weight', 'symbol'];
+  dataSource = new MatTableDataSource(this.candidateList);
+
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  createCandidateSubmit(){
+
+  }
+
+  isInvalidField(field: string){
+    return this.createCandidateForm.get(field)?.invalid && (this.createCandidateForm.get(field)?.dirty || this.createCandidateForm.get(field)?.touched);
+  }
+
+  hasError(field: string, validation: string){
+    return this.createCandidateForm.get(field)?.hasError(validation);
+  }
+
+  applyFilter(event: Event) {
+    const filterValue = (event.target as HTMLInputElement).value;
+    this.dataSource.filter = filterValue.trim().toLowerCase();
+  }
+
 }
+
